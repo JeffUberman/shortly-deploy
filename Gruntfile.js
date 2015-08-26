82e2afc91d700e5cff90bc19ababfcee37c1bdde
Glenn Meyer <glennmeyer@gmail.com>
2015-08-26 10:09:38 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,121 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      //your code here
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec' //nyan, xunit, html-cov, dot, min, markdown
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    uglify: {
+      //your code here
+    },
+
+    jshint: {
+      files: [
+        // Add filespec list here
+      ],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      }
+    },
+
+    cssmin: {
+      //your code here
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'public/*.css',
+        tasks: ['cssmin']
+      },
+      server: {
+        //your code here
+      }
+    },
+
+    shell: {
+      prodServer: {
+        //can be used to auto-deploy to Heroku/Azure.
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+  ]);
+
+  //can be used to auto-deploy.
+  grunt.registerTask('upload', function(n) {
+    //Grunt options are ways to customize tasks.  Research ways to use them.
+    if(grunt.option('prod')) {
+      // add your production server task here
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    // add your deploy tasks here
+  ]);
+
+
+};
---
+++ app/models/link.solution.js
@@ -0,0 +1,28 @@
+var db = require('../config');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var linkSchema = mongoose.Schema({
+ visits: Number,
+ link: String,
+ title: String,
+ code: String,
+ base_url: String,
+ url: String
+});
+
+var Link = mongoose.model('Link', linkSchema);
+
+var createSha = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function(next){
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+});
+
+module.exports = Link;
